13f5e21c53ff262812f95ddd85f00e5de2d67d8e
Max Miller <maxmiller413@gmail.com>
2021-01-31 20:18:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,8 @@ def load_library(path)
   final_hash = {}
   YAML.load_file(path).each do |k, v|
     final_hash[k] = {}
-    final_hash[k][:english]
-    final_hash[k][:japanese]
+    final_hash[k][:english] = v[0]
+    final_hash[k][:japanese] = v[1]
   binding.pry 
   end 
   final_hash
