71442a6e19e3e5a9a9aa90c0b05067fa520adacd
Max Miller <maxmiller413@gmail.com>
2021-01-31 20:15:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,9 +4,9 @@ require 'pry'
 
 def load_library(path)
   final_hash = {}
-  YAML.load_file(path)
-  #binding.pry 
-  #end 
+  YAML.load_file(path).each do |k, v|
+  binding.pry 
+  end 
   final_hash
 end
 
